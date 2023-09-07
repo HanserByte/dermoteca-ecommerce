7fa36f90cf3ab9dbc4180d897b968b3120ff86f7
Hector Garcia <hector032716@gmail.com>
2023-09-06 17:05:43 -0700

    ⭐ Add responsive breakpoints

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,11 +1,17 @@
 import ProductCard from "@/components/ProductCard";
-import { Grid } from "@chakra-ui/react";
+import { useStore } from "@/store";
+import { Grid, useMediaQuery } from "@chakra-ui/react";
 import React from "react";
 
 const test = () => {
+  const { value } = useStore();
+  const [isMobile] = useMediaQuery(`(max-width: ${value})`);
   return (
     <>
-      <Grid templateColumns="repeat(4, 1fr)" gap={3}>
+      <Grid
+        templateColumns={isMobile ? "repeat(2, 1fr)" : "repeat(4, 1fr)"}
+        gap={3}
+      >
         <ProductCard
           imageSrc="https://images.unsplash.com/photo-1615396899839-c99c121888b0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
           title="Physiopure Dúo"
@@ -28,7 +34,10 @@ const test = () => {
         />
       </Grid>
 
-      <Grid templateColumns="repeat(3, 1fr)" gap={3}>
+      <Grid
+        templateColumns={isMobile ? "repeat(2, 1fr)" : "repeat(3, 1fr)"}
+        gap={3}
+      >
         <ProductCard
           imageSrc="https://images.unsplash.com/photo-1615396899839-c99c121888b0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
           title="Physiopure Dúo"
