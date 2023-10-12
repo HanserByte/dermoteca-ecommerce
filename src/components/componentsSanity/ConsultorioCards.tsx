27042c87e21b0ac7cb40e6fc7ef782a5ffe92aea
Hector Garcia <hector032716@gmail.com>
2023-10-12 12:50:10 -0700

    ⭐ Add responsive consultorio cards

--- src/components/componentsSanity/ConsultorioCards.tsx
+++ src/components/componentsSanity/ConsultorioCards.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ContainerDermo from "../Common/ContainerDermo";
-import { Box, Grid, HStack, Text, VStack } from "@chakra-ui/react";
+import { Box, Grid, HStack, Stack, Text, VStack } from "@chakra-ui/react";
 import PortableText from "../PortableText";
 import { COLORS } from "@/utils/constants";
 import Link from "next/link";
+import { useMobileView } from "@/hooks/responsive";
 
 interface IProps {
   data: {
@@ -20,17 +21,26 @@ interface IProps {
 }
 
 const ConsultorioCards = ({ data }: IProps) => {
+  const { isMobile } = useMobileView();
   return (
     <ContainerDermo pt="37px" pb="37px">
-      <HStack gap={5} alignItems="start">
-        <VStack alignItems="start" w="40%">
+      <Stack
+        gap={5}
+        alignItems="start"
+        flexDirection={isMobile ? "column" : "row"}
+      >
+        <VStack alignItems="start" w={isMobile ? "auto" : "40%"}>
           <Text fontWeight={700} fontSize="xl">
             {data?.title}
           </Text>
           <PortableText blocks={data?.subtitle} />
         </VStack>
 
-        <Grid w="full" templateColumns={"repeat(2, 1fr)"} gap={5}>
+        <Grid
+          w="full"
+          templateColumns={isMobile ? "1fr" : "repeat(2, 1fr)"}
+          gap={5}
+        >
           {data?.cards?.map((card) => (
             <VStack alignItems="start" borderRadius={10} p={50} bg="#F8F9FA">
               <Text fontWeight={500} fontSize="lg">
@@ -40,7 +50,7 @@ const ConsultorioCards = ({ data }: IProps) => {
               <Text
                 _hover={{ textDecoration: "underline" }}
                 as={Link}
-                href={card?.url}
+                href={`${card?.url.includes("@") ? "mailto:" : ""}${card?.url}`}
                 color={COLORS.GREEN}
                 fontWeight={600}
                 fontSize="sm"
@@ -54,7 +64,7 @@ const ConsultorioCards = ({ data }: IProps) => {
             </VStack>
           ))}
         </Grid>
-      </HStack>
+      </Stack>
     </ContainerDermo>
   );
 };
