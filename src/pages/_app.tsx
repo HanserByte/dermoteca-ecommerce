20cbf132a5bd871fbfb3ef7ac8a722ecde68ba3a
Osvaldo Hernandez <65359113+hecntdev@users.noreply.github.com>
2023-11-11 13:58:37 -0600

    fix score

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,6 +26,11 @@ function MyApp({ Component, pageProps }: any) {
       <Head>
         <title>Dermoteca</title>
         <link rel="icon" href="/favicon.ico?v=2" />
+        <meta
+          name="description"
+          content="Explora soluciones dermatológicas personalizadas para el cuidado de tu piel. Nuestro equipo de dermatólogos expertos ofrece tratamientos especializados para afecciones cutáneas, procedimientos estéticos y consejos de cuidado de la piel. Descubre una piel saludable y radiante con nuestros servicios de dermatología de vanguardia."
+        />
+        <html lang="es" />
       </Head>
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
