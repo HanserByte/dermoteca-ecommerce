ea9cde690261d90e55bfae00af239ab77848841f
Jose Carlos Hernandez <josecarlos@innomius.com>
2025-09-16 18:45:08 -0600

    add draft mode

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -3,12 +3,14 @@ import { createClient } from "next-sanity";
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
-const SANITY_TOKEN = process.env.NEXT_PUBLIC_TOKEN_SANITY
+const SANITY_TOKEN = process.env.NEXT_PUBLIC_TOKEN_SANITY;
+const SANITY_MODE = process.env.NEXT_PUBLIC_SANITY_MODE;
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: false,
-  token: SANITY_TOKEN
+  token: SANITY_TOKEN,
+  perspective: SANITY_MODE === "production" ? "published" : "previewDrafts",
 });
